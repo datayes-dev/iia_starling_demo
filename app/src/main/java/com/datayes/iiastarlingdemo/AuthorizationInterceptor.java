e2e95cff0d5f266f72a846151ab15716952a74e0
Shenen.Gao <shenen.gao@datayes.com>
2018-05-09 19:13:52 +0800

    临时提交

--- app/src/main/java/com/datayes/iiastarlingdemo/AuthorizationInterceptor.java
+++ app/src/main/java/com/datayes/iiastarlingdemo/AuthorizationInterceptor.java
@@ -20,7 +20,7 @@ public class AuthorizationInterceptor implements Interceptor {
         Request.Builder builder = chain.request().newBuilder();
 
         // TODO 用户id
-        builder.addHeader("DatayesPrincipalName", "11111111");
+        builder.addHeader("DatayesPrincipalName", Test.INSTANCE.getTestUserId());
 
         // TODO deviceId
         builder.addHeader("deviceId", "deviceId");
